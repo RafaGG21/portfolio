8923f479c824438d75d120b8e9d7df66d4e60d4e
Rafa <rafagandolfogarcia@gmail.com>
2025-06-01 13:45:54 +0200

    toggle e idiomas

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-
+import { CustomTranslateLoader } from './services/translate-loader';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar';
 import { AboutMe } from './pages/about-me/about-me';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -17,8 +23,17 @@ import { AboutMe } from './pages/about-me/about-me';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useClass: CustomTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   bootstrap: [App]
 })
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'navbar',
@@ -10,9 +11,34 @@ export class NavbarComponent {
   selectedLanguage: string = 'es';
   darkMode: boolean = false;
   @Output() darkModeOutput = new EventEmitter<boolean>();
+  @Output() selectedLanguageOutput = new EventEmitter<string>();
 
-  changeLanguage(lang: string) {
-    this.selectedLanguage = lang;
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es'); // idioma por defecto
+  }
+
+  dropdownOpen = false;
+
+toggleDropdown() {
+  this.dropdownOpen = !this.dropdownOpen;
+}
+
+  languages = [
+    { code: 'es', name: 'Español', flag: 'assets/flags/bandera_spain.png' },
+    { code: 'fr', name: 'Français', flag: 'assets/flags/bandera_france.png' },
+    { code: 'en', name: 'English', flag: 'assets/flags/bandera_uk.png' }
+  ];
+
+  getLanguageName(code: string): string {
+    const lang = this.languages.find(l => l.code === code);
+    return lang ? lang.name : '';
+  }
+
+  changeLanguage(langCode: string) {
+
+    this.selectedLanguage = langCode;
+    this.translate.use(this.selectedLanguage);
   }
 
   toggleDarkMode() {
