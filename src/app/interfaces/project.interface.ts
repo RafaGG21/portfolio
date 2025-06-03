8938361421b9a45c7561c9128ab4fe851fef926f
Rafa <rafagandolfogarcia@gmail.com>
2025-06-03 20:31:49 +0200

    data

--- src/app/interfaces/project.interface.ts
+++ src/app/interfaces/project.interface.ts
@@ -2,6 +2,8 @@ export interface Project {
   id: number;
   name: string;
   images: string[];
-  description: string;
   technologies: string[];
+  description_es: string;
+  description_en: string,
+  description_fr: string;
 }
