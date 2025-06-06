20c58199f3a8ff5873f94b9d3597930343103126
Rafa <rafagandolfogarcia@gmail.com>
2025-06-06 09:21:03 +0200

    animation skills

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { ISkill } from '../../interfaces/skill.interface';
 
 @Component({
@@ -7,7 +7,7 @@ import { ISkill } from '../../interfaces/skill.interface';
   styleUrl: './skills.css',
   standalone: false
 })
-export class Skills {
+export class Skills implements AfterViewInit{
   skills: ISkill[] = [
     { name: 'Angular', level: 60 },
     { name: 'Java', level: 70 },
@@ -21,4 +21,26 @@ export class Skills {
     { name: 'PHP', level: 40 },
     { name: 'Docker', level: 40 }
   ];
+
+  @ViewChildren('skillFill') skillFills!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const el = entry.target as HTMLElement;
+          const percent = el.getAttribute('data-percent');
+
+          if (entry.isIntersecting && percent) {
+            el.style.width = `${percent}%`;
+          } else {
+            el.style.width = '0%'; // reinicia para reanimar cada vez
+          }
+        });
+      },
+      { threshold: 0.6 }
+    );
+
+    this.skillFills.forEach((el) => observer.observe(el.nativeElement));
+  }
 }
